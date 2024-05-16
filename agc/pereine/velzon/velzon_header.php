dba3b44b0ff9a179b6f9c5e60f2888c3c740e418
root <root@vmi1704593.contaboserver.net>
2024-05-16 14:59:41 +0200

    commit

--- agc/pereine/velzon/velzon_header.php
+++ agc/pereine/velzon/velzon_header.php
@@ -111,7 +111,7 @@
               <div class="dropdown ms-sm-3 header-item topbar-user">
                 <button type="button" class="btn material-shadow-none" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <span class="d-flex align-items-center">
-                    <img class="rounded-circle header-profile-user" src="../agc/pereine/velzon/assets/images/users/avatar-1.jpg" alt="Header Avatar">
+                    <img class="rounded-circle header-profile-user" src="../abdellah/agc/pereine/velzon/assets/images/users/avatar-1.jpg" alt="Header Avatar">
                     <span class="text-start ms-xl-2">
                       <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">Agent Screen</span>
                       <span class="d-none d-xl-block ms-1 fs-12 user-name-sub-text"><?php echo "$VD_login-$SIP_user" ?></span>
